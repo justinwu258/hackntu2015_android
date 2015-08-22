390ad86356cd4e1be10a31210f0fcbbf1ae0faab
JUSTINWU <justinwu258@hotmail.com>
2015-08-23 07:46:59 +0800

    [hackntu_android] make horizontalScrollView OK

--- SmartStation/app/src/main/java/project/hackthon/ntu/smartstation/MainActivity.java
+++ SmartStation/app/src/main/java/project/hackthon/ntu/smartstation/MainActivity.java
@@ -1,6 +1,7 @@
 package project.hackthon.ntu.smartstation;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,6 +15,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -174,6 +177,13 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
             final TextView right_box_mail_num = (TextView) rootView.findViewById(R.id.right_box_mail_num_post);
             final TextView right_box_mail_weight = (TextView) rootView.findViewById(R.id.right_box_mail_weight_post);
             //??search_button??
+            if(rootView !=null){
+                postbox_id.requestFocus();
+                getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+//                InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+//                imm.hideSoftInputFromWindow(rootView.getWindowToken(),0);
+            }
+
             Button search = (Button) rootView.findViewById(R.id.search_button);
 
             //search_button??
